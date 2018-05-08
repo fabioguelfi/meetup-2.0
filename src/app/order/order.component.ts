e85f72ab7417bbbbafd5be946995363577d94859
Fabio Guelfi <fabioguelfiunix@gmail.com>
2018-05-07 22:30:14 -0300

    Criando Componente Usando ControlValueAccessor (Parte 1)

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderComponent implements OnInit {
 
+  public paymentOptions = [
+    { label: 'Dinheiro', value: 'MON' },
+    { label: 'Cartão de Débito', value: 'DEB' },
+    { label: 'Cartão Refeição', value: 'REF' },
+  ];
+
   constructor() { }
 
   ngOnInit() {
