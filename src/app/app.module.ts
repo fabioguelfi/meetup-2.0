f2e0c47b3ab19a6f5fd7e7f3e720f842c2db1267
Fabio Guelfi <fabioguelfiunix@gmail.com>
2018-05-11 08:42:16 -0300

    Criando Um Módulo Compartilhado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,13 +19,13 @@ import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.compo
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import { ShoppingCartService } from './services/shopping-cart.service';
 import { OrderComponent } from './order/order.component';
-import { InputComponent } from './shared/input/input.component';
-import { RadioComponent } from './shared/radio/radio.component';
 import { OrderItemsComponent } from './order/order-items/order-items.component';
 import { OrderService } from './services/order.service';
 import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
 import { OrderSummaryComponent } from './order/order-summary/order-summary.component';
-import { RatingComponent } from './shared/rating/rating.component';
+
+// shared module
+import { SharedModule } from './shared/shared/shared.module';
 
 
 @NgModule({
@@ -41,19 +41,17 @@ import { RatingComponent } from './shared/rating/rating.component';
     MenuItemComponent,
     ReviewsComponent,
     OrderComponent,
-    InputComponent,
-    RadioComponent,
     OrderItemsComponent,
     DeliveryCostsComponent,
     OrderSummaryComponent,
-    RatingComponent,
   ],
   imports: [
-    BrowserModule,
+  BrowserModule,
     HttpModule,
     RouterModule,
     FormsModule,
     ReactiveFormsModule,
+    SharedModule,
     RouterModule.forRoot(ROUTES),
   ],
   providers: [
